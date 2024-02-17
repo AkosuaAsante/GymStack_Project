3d41132b02d232930e62874c0d3ed5dcdfd84566
AkosuaAsante <akosdedaa@gmail.com>
2024-02-17 14:24:33 +0000

    complete regiter

--- public/login/Register1.php
+++ public/login/Register1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Register- Task Management System</title>
-    <link rel="stylesheet" href="Register.css" />
+    <link rel="stylesheet" href="../css/Register.css" />
   </head>
   <body>
     <div class="login-container">
@@ -12,32 +12,23 @@
         <img class="logo" src="../images/logo_transparent.png" alt="">
         <span class="logo-text">GYM STACK</span>
       </div>
-      
       <h2 id="welcome-back">Register Here</h2>
-      <form method="POST" onsubmit="return validateLogin()" id="login-form" name="login-form" >
-        <div>
-           <input type="text" id="fname" name="fname" placeholder="John" required />
-           <input type="text" id="lname" name="lname" placeholder="Doe" required />
+      <form class="form-background" method="POST" onsubmit="return validateLogin()" id="login-form" name="login-form" >
+          <input type="text" id="fname" name="fname" placeholder="John" required />
+          <input type="text" id="lname" name="lname" placeholder="Doe" required />
           <input type="text" id="email" name="email" placeholder="email address" required />
           <input type="password" id="password" name="password" placeholder="password" required />
           <input type="password" id="confirmpassword" name="confirmpassword" placeholder="confirm password" required />
           <input type="text" class="form-control" id="gymname" placeholder="Planet Fitness">
           <input type="text" class="form-control" id="Address" placeholder="123 St Monica Street">
                   <select id="role" class="form-select">
-                    <option>Role</option>
-                    <option>Manager</option>
-                    <option>Customer</option>
+                    <option value=0>Role</option>
+                    <option value=1>Manager</option>
+                    <option value=2>Customer</option>
                   </select>
-
+            <button type="submit" name="sign-in" id="sign-in" onclick="validate()">Sign Up</button>
           <p class ="error-msg" id="error-msg"> Incorrect email or password. Try again</p>
-        </div>
-        <button type="submit" name="sign-in" id="sign-in" onclick="validate()">Sign Up</button>
-        
       </form>
-        
-      </div>
-      
-    </div>
     </div>
     <script src="../js/loginpage.js"></script>
   </body>
