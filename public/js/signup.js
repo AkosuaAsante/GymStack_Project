10f1201a36055cca0869cc9908dfe71415ce9b20
AkosuaAsante <akosdedaa@gmail.com>
2024-02-18 23:55:58 +0000

    js changes

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,12 +5,13 @@
         const phoneRegex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
 
         function validateRegister(){
-            var firstName = document.getElementById("fname").value;
-            var lastName = document.getElementById("lname").value;
-            var emailaddress = document.getElementById("email").value;
-            var password = document.getElementById("password").value;
-            var confirmPassword = document.getElementById("confirmPassword").value;
-            var phone = document.getElementById("phoneNumber").value;
+            event.preventDefault();
+            var firstName = document.getElementById("fname");
+            var lastName = document.getElementById("lname");
+            var emailaddress = document.getElementById("email");
+            var password = document.getElementById("password");
+            var confirmPassword = document.getElementById("confirmPassword");
+            var phone = document.getElementById("phoneNumber");
         
             if (!nameRegex.test(firstName)) {
                 alert("Invalid first name");
@@ -18,35 +19,36 @@
                 return false;
             } 
             
-            else if (!nameRegex.test(lastName)) {
+            if (!nameRegex.test(lastName)) {
                 document.getElementById('lname-error-msg').style.display = 'block';
                 return false;
             }
 
-            else if (!phoneRegex.test(phone)) {
+            if (!phoneRegex.test(phone)) {
                 document.getElementById('phone-error-msg').style.display = 'block';
                 return false;
             }
 
-            else if (!emailRegex.test(emailaddress)) {
+            if (!emailRegex.test(emailaddress)) {
                 document.getElementById('email-error-msg').style.display = 'block';
                 return false;
             }
             
-            else if (!passwordRegex.test(password)) {
+            if (!passwordRegex.test(password)) {
                 document.getElementById('password-error-msg').style.display = 'block';
                 return false;
             }
 
-            else if (password !== confirmPassword) {
+             if (password !== confirmPassword) {
                 document.getElementById('confirmpassword-error-msg').style.display = 'block';
                 return false;
             }
 
-            else {
+            
                 alert("Form submitted successfully!");
-                window.location.href = "login/Register1.php";
-            }
+                window.location.href = "../login/Register1.php";
+                return false;
+            
 
         }
            
