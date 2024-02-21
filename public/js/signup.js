435d52bf67cc6e6351788dd4e8384807b54d5bc2
AkosuaAsante <akosdedaa@gmail.com>
2024-02-21 01:04:51 +0000

    new js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,62 +1,37 @@
 
+
+function showError(element,message) {
+        document.getElementById(element).textContent = message;  
+        document.getElementById(element).style.display = "block"; 
+      }
+       
+
+function validateRegister(){
+        event.preventDefault();
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
         const nameRegex =  /^[a-zA-Z- ]+$/;
         const phoneRegex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
 
-        function showError(message) {
-            document.getElementById("error-msg").textContent = message;  
-            document.getElementById("error-msg").style.display = "block"; 
-          }
-          
+        var fname=document.getElementById("fname");
+        var lname=document.getElementById("lname");
+        var email=document.getElementById("email");
+        var password=document.getElementById("password");
+        var confirmPassword=document.getElementById("confirmPassword");
 
-        function validateRegister(){
-            event.preventDefault();
-            var firstName = document.getElementById("fname").value;
-            var lastName = document.getElementById("lname").value;
-            var emailaddress = document.getElementById("email").value;
-            var password = document.getElementById("password").value;
-            var confirmPassword = document.getElementById("confirmPassword").value;
-            var phone = document.getElementById("phoneNumber").value;
-        
-            if (!nameRegex.test(firstName)) {
-                showError("Invalid first name. Please use letters, spaces, and hyphens.");
-                return false;
-            } 
-            
-            if (!nameRegex.test(lastName)) {
-                showError("Invalid last name. Please use letters, spaces, and hyphens.");
-                return false;
-            }
 
-            if (!phoneRegex.test(phone)) {
-                document.getElementById('phone-error-msg').style.display = 'block';
+         if (!nameRegex.test(fname)) {
+                showError("fname-error-msg","Invalid first name. Please use letters, spaces, and hyphens.");
                 return false;
-            }
-
-            if (!emailRegex.test(emailaddress)) {
-                document.getElementById('email-error-msg').style.display = 'block';
+        } 
+        else if (!nameRegex.test(lname)) {
+                showError("lname-error-msg","Invalid last name. Please use letters, spaces, and hyphens.");
                 return false;
-            }
-            
-            if (!passwordRegex.test(password)) {
-                document.getElementById('password-error-msg').style.display = 'block';
-                return false;
-            }
-
-             if (password !== confirmPassword) {
-                document.getElementById('confirmpassword-error-msg').style.display = 'block';
-                return false;
-            }
-
-            
-                alert("Form submitted successfully!");
-                window.location.href = "../login/Register1.php";
-                return false;
-            
-
         }
-           
+
+}
+
+
 
         
 
--- public/login/Register1.php
+++ public/login/Register1.php
@@ -15,23 +15,29 @@
       <h2 id="welcome-back">Register Here</h2>
       <form class="form-background" method="POST" onsubmit="return validateRegister()" id="register-form" name="register-form" >
           <input type="text" id="fname" name="fname" placeholder="Enter First Name" required />
-          <p class ="error-msg" id="error-msg"> Incorrect Name . Try again</p>
+          <p class ="error-msg" id="fname-error-msg"> Incorrect Name . Try again</p>
+
           <input type="text" id="lname" name="lname" placeholder="Enter Last Name" required />
-          <p class ="error-msg" id="error-msg"> Incorrect Name . Try again</p>
+          <p class ="error-msg" id="lname-error-msg"> Incorrect Name . Try again</p>
+
           <input type="email" id="email" name="email" placeholder="Email Address" required />
-          <p class ="error-msg" id="error-msg"> Incorrect Email Address . Try again</p>
+          <p class ="error-msg" id="email-error-msg"> Incorrect Email Address . Try again</p>
+
           <input type="password" id="password" name="password" placeholder="Password" required />
-          <p class ="error-msg" id="error-msg"> Incorrect Password . Try again</p>
+          <p class ="error-msg" id="password-error-msg"> Incorrect Password . Try again</p>
+
           <input type="password" id="confirmpassword" name="confirmPassword" placeholder="Confirm Password" required />
-          <p class ="error-msg" id="error-msg"> Password does not match. Try again</p>
+          <p class ="error-msg" id="cpassword-error-msg"> Password does not match. Try again</p>
+
           <input type="text" class="form-control" id="gymname" placeholder="Gym Name">
+
           <input type="number" class="form-control" id="phoneNumber" placeholder="Phone Number">
-                  <select id="role" class="form-select">
-                    <option value=0>Role</option>
-                    <option value=1>Manager</option>
-                    <option value=2>Customer</option>
+                  <select id="role" class="form-select" name="role" title="role">
+                    <option name="role"value=0>Role</option>
+                    <option name="role" value=1>Manager</option>
+                    <option name="role" value=2>Customer</option>
                   </select>
-            <button type="submit" name="sign-in" id="sign-in">Sign Up</button>
+            <button type="submit" name="sign-in" id="sign-in" onclick="return validateRegister()">Sign Up</button>
       </form>
     </div>
     <script defer src="../js/signup.js"></script>
