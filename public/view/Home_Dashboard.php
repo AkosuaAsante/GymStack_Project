049ec44d5c66e9a71f3d0adfc0d5cbef8bc563b4
FaithRovina <faithrovina14@gmail.com>
2024-03-12 00:31:08 +0000

    added logout

--- public/view/Home_Dashboard.php
+++ public/view/Home_Dashboard.php
@@ -65,7 +65,7 @@
                 </li>
             </ul>
             <div class="sidebar-footer">
-                <a href="#" class="sidebar-link">
+                <a href="../login/logout.php" class="sidebar-link">
                     <i class="lni lni-exit"></i>
                     <span>Log Out</span>
                 </a>
